e16b3f4493e1d2ae495cd35ed2c97490ca2681e0
Zach Leatherman <zachleatherman@gmail.com>
2020-02-24 20:18:59 -0600

    Adds more exports

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
 const EleventyNavigation = require("./eleventy-navigation");
 
+// export the configuration function for plugin
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addNunjucksFilter("eleventyNavigation", EleventyNavigation.findNavigationEntries);
 	eleventyConfig.addNunjucksFilter("eleventyNavigationBreadcrumb", EleventyNavigation.findBreadcrumbEntries);
 	eleventyConfig.addNunjucksFilter("eleventyNavigationToHtml", function(pages, options) {
 		return EleventyNavigation.toHtml.call(eleventyConfig, pages, options);
 	});
+};
+
+module.exports.navigation = {
+	find: EleventyNavigation.findNavigationEntries,
+	findBreadcrumbs: EleventyNavigation.findBreadcrumbEntries,
 };
