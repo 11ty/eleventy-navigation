33322c73088adf937ed1f588a3f082a34182aa86
mstenz <mstenz-design@web.de>
2021-08-19 15:56:41 +0200

    redo unwanted changes

--- eleventy-navigation.js
+++ eleventy-navigation.js
@@ -65,15 +65,33 @@ function getDependencyGraph(nodes) {
 	return graph;
 }
 
-function findBreadcrumbEntries(nodes, activeKey) {
+function findBreadcrumbEntries(nodes, activeKey, options = {}) {
 	let graph = getDependencyGraph(nodes);
+	let deps = graph.dependenciesOf(activeKey);
+	if(options.includeSelf) {
+		deps.push(activeKey);
+	}
 
-	return activeKey ? graph.dependenciesOf(activeKey).map(key => {
+	return activeKey ? deps.map(key => {
 		let data = Object.assign({}, graph.getNodeData(key));
 		delete data.children;
 		data._isBreadcrumb = true;
 		return data;
 	}) : [];
+}
+
+function getUrlFilter() {
+	if("getFilter" in this) {
+		// v0.10.0 and above
+		return this.getFilter("url");
+	} else if("nunjucksFilters" in this) {
+		// backwards compat, hardcoded key
+		return this.nunjucksFilters.url;
+	} else {
+		// Theoretically we could just move on here with a `url => url` but then `pathPrefix`
+		// would not work and it wouldn’t be obvious why—so let’s fail loudly to avoid that.
+		throw new Error("Could not find a `url` filter for the eleventy-navigation plugin in eleventyNavigationToHtml filter.");
+	}
 }
 
 function navigationToHtml(pages, options = {}) {
@@ -94,18 +112,7 @@ function navigationToHtml(pages, options = {}) {
 	let isChildList = !!options.isChildList;
 	options.isChildList = true;
 
-	let urlFilter;
-	if("getFilter" in this) {
-		// v0.10.0 and above
-		urlFilter = this.getFilter("url");
-	} else if("nunjucksFilters" in this) {
-		// backwards compat, hardcoded key
-		urlFilter = this.nunjucksFilters.url;
-	} else {
-		// Theoretically we could just move on here with a `url => url` but then `pathPrefix`
-		// would not work and it wouldn’t be obvious why—so let’s fail loudly to avoid that.
-		throw new Error("Could not find a `url` filter for the eleventy-navigation plugin in eleventyNavigationToHtml filter.");
-	}
+	let urlFilter = getUrlFilter.call(this);
 
 	if(pages.length && pages[0].pluginType !== "eleventy-navigation") {
 		throw new Error("Incorrect argument passed to eleventyNavigationToHtml filter. You must call `eleventyNavigation` or `eleventyNavigationBreadcrumb` first, like: `collection.all | eleventyNavigation | eleventyNavigationToHtml | safe`");
@@ -131,14 +138,35 @@ function navigationToHtml(pages, options = {}) {
 		if(options.listItemHasChildrenClass && entry.children && entry.children.length) {
 			liClass.push(options.listItemHasChildrenClass);
 		}
-
 		return `<${options.listItemElement}${liClass.length ? ` class="${liClass.join(" ")}"` : ''}><a href="${urlFilter(entry.url)}"${aClass.length ? ` class="${aClass.join(" ")}"` : ''}>${entry.title}</a>${options.showExcerpt && entry.excerpt ? `: ${entry.excerpt}` : ""}${entry.children ? navigationToHtml.call(this, entry.children, options) : ""}</${options.listItemElement}>`;
 	}).join("\n")}</${options.listElement}>` : "";
+}
+
+function navigationToMarkdown(pages, options = {}) {
+	options = Object.assign({
+		showExcerpt: false,
+		childDepth: 0
+	}, options);
+
+	let childDepth = 1 + options.childDepth;
+	options.childDepth++;
+
+	let urlFilter = getUrlFilter.call(this);
+
+	if(pages.length && pages[0].pluginType !== "eleventy-navigation") {
+		throw new Error("Incorrect argument passed to eleventyNavigationToMarkdown filter. You must call `eleventyNavigation` or `eleventyNavigationBreadcrumb` first, like: `collection.all | eleventyNavigation | eleventyNavigationToMarkdown | safe`");
+	}
+
+	let indent = (new Array(childDepth)).join("  ") || "";
+	return pages.length ? `${pages.map(entry => {
+		return `${indent}* [${entry.title}](${urlFilter(entry.url)})${options.showExcerpt && entry.excerpt ? `: ${entry.excerpt}` : ""}\n${entry.children ? navigationToMarkdown.call(this, entry.children, options) : ""}`;
+	}).join("")}` : "";
 }
 
 module.exports = {
 	getDependencyGraph,
 	findNavigationEntries,
 	findBreadcrumbEntries,
-	toHtml: navigationToHtml
+	toHtml: navigationToHtml,
+	toMarkdown: navigationToMarkdown
 };
